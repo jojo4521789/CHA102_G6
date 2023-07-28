6a3c25818704c0708a67b1bfe75afef5b82ce81c
LAPTOP-EGI1T3KO\T14 Gen 3 <chouyuming1994@gmail.com>
2023-07-28 18:22:30 +0800

    test

--- src/main/java/cornnn.java
+++ src/main/java/cornnn.java
@@ -1,7 +1,7 @@
 
 public class cornnn {
 	public static void main(String[] args) {
-		System.out.println("安安我是玉米");
-		System.out.println("安安我是玉米");
+		System.out.println("安安");
+		System.out.println("安安2");
 	}
 }
